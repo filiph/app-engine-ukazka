a55252775cec1a606394d158ea9a1aca494bf4f1
Filip Hracek <filiph@google.com>
2014-03-12 14:34:03 +0100

    Add Jinja templating

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 
 import webapp2
+from jinja import render_html
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(u'Ahoj Česko!')
+        render_html(self, "list.html", u"Ahoj",
+                    u"Toto je super kůlový.")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
