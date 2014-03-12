a820bade6074cb5a95f2741f7382dbca0c147efe
Filip Hracek <filiph@google.com>
2014-03-12 14:55:49 +0100

    Add login

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 import webapp2
 from jinja import render_html
 
+from google.appengine.api import users
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        render_html(self, "list.html", u"Ahoj",
-                    u"Toto je super kůlový.")
+        user = users.get_current_user()
+        if user:
+            render_html(self, "list.html", u"Ahoj",
+                        u"Nejsi ty náhodou {}?".format(user.nickname()))
+        else:
+            render_html(self, "list.html", u"Ahoj",
+                        u"<a href='{}'>Přihlaš se</a>.".format(users.create_login_url("/")))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
