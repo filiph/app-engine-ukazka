30aefc0aa942fb125a0ca0d07634c10b347b593a
Filip Hracek <filiph@google.com>
2014-03-12 15:12:11 +0100

    Rename field

--- scientist.py
+++ scientist.py
@@ -4,5 +4,5 @@ from google.appengine.ext import ndb
 class Scientist(ndb.Model):
     name = ndb.StringProperty()
     surname = ndb.StringProperty()
-    year_of_birth = ndb.DateProperty()
+    birth_date = ndb.DateProperty()
 
